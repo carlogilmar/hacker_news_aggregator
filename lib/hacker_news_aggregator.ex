7017f540b6d730cedd505b7852aba59249e774cd
Carlo Gilmar <carlogilmar12@gmail.com>
2020-09-28 22:15:08 -0500

    Adding handle_response to manage code_status in api

--- lib/hacker_news_aggregator.ex
+++ lib/hacker_news_aggregator.ex
@@ -1,20 +1,38 @@
 defmodule HackerNewsAggregator do
   @moduledoc """
-    This module is for provide a general way to interact to the core functionality
+    This module is for provide a general way to interact from the API to the core functionality
     - Get a story by id
     - Get a chunk of stories
   """
   alias HackerNewsAggregator.StoryClient
 
   @doc "Function to get a story by id"
-  @spec get_story_by_id(String.t()) :: struct()
-  def get_story_by_id(story_id) do
-    StoryClient.get_story(story_id)
+  @spec get_story_by_id(String.t()) :: {200, struct()} | {404, map()}
+  def get_story_by_id(story_id) when is_binary(story_id) do
+    story_id
+    |> StoryClient.get_story()
+    |> handle_response()
   end
 
   @doc "Function for get a chunk of paginated stories"
-  @spec get_stories(integer()) :: list()
-  def get_stories(page) do
-    StoryClient.get_stories(page)
+  @spec get_stories(String.t()) :: {200, list()} | {404, map()}
+  def get_stories(page) when is_binary(page) do
+    page
+    |> String.to_integer()
+    |> StoryClient.get_stories()
+    |> handle_response()
+  end
+
+  defp handle_response(resource) do
+    case resource do
+      nil ->
+        {404, %{msg: "Resource not found"}}
+
+      [] ->
+        {404, %{msg: "Resource not found"}}
+
+      resource ->
+        {200, resource}
+    end
   end
 end
